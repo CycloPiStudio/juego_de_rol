d1ebd43e460273ac5243a285df74d5c3366f4205
brecabre <bravomantonio@gmail.com>
2021-06-26 23:20:26 +0200

    Add files via upload

---
+++ juego rol/globales/GlobalServer.gd
@@ -0,0 +1,24 @@
+extends Node
+var peer = NetworkedMultiplayerENet.new()
+onready var SERVER_IP = "1"
+onready var SERVER_PORT = 6969
+onready var MAX_PLAYERS = 3
+
+func create_server(SERVER_PORT):
+	
+	peer.create_server(SERVER_PORT, MAX_PLAYERS)
+	get_tree().network_peer = peer
+	print("servidor iniciado")
+	
+	peer.connect("peer_connected", self, "_Peer_Connected")
+	peer.connect("peer_disconnected", self, "_Peer_Disconnected")
+
+
+
+func create_client(SERVER_IP, SERVER_PORT):
+	
+	peer.create_client(SERVER_IP, SERVER_PORT)
+	get_tree().network_peer = peer
+	peer.connect("connection_failed", self, "_OnConnectionFailed")
+	peer.connect("connection_succeeded", self, "_OnConnectionSucceeded")
+	peer.connect("server_disconnected" , self, "_OnServerDisconnected")
